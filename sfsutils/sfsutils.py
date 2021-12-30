19c8a15828ccadd6352de1fc310010563698670e
mark9064 <30447455+mark9064@users.noreply.github.com>
2021-12-30 15:27:23 +0000

    Refactor parse and serialise for increased performance and compatability with single letter nodes

--- sfsutils/sfsutils.py
+++ sfsutils/sfsutils.py
@@ -1,7 +1,8 @@
 """Parser for KSP save file"""
-from collections import OrderedDict
+from typing import Any, Dict, List, Optional, Union
 
-def parse_savefile(sfs, sfs_is_path=True):
+
+def parse_savefile(sfs: str, sfs_is_path: bool = True) -> Dict[str, Any]:
     """Parses an SFS file
 
     Params:
@@ -10,15 +11,15 @@ def parse_savefile(sfs, sfs_is_path=True):
     Raises:
         No specific exceptions.
     Returns:
-        OrderedDict containing the data in the SFS.
+        Dictionary containing the data in the SFS.
     Extra information:
         All values are strings as SFS files do not reveal data to be any type.
-        The SFS format is particularly bad and this leads to the returned OrderedDict
+        The SFS format is particularly bad and this leads to the returned dictionary
         containing data that is unusually structured. If the SFS contains multiple keys of any
         kind with the same name (this can be a 'node' header or values in a node), then the data
         contained within these keys will formatted as the common name of the keys as a key
         in a dict, and the values as a list. This data will always be in the exact order
-        that they were in in the SFS. Example:
+        that they were in in the SFS (dictionaries are ordered in python3.6+). Example:
 
         --SFS format--
         NODE
@@ -48,59 +49,59 @@ def parse_savefile(sfs, sfs_is_path=True):
         }
     """
     if sfs_is_path:
-        data = open(sfs, "r").read()
+        with open(sfs, "r") as file:
+            data = file.read()
     else:
         data = sfs
-    # removes all tabs
-    data = data.replace("\t", "")
+    # removes all tabs, spaces after newlines and spaces around equals
+    data = data.replace("\t", "").replace("\n ", "\n").replace(" = ", "=")
     # in_nodes tracks the location of data being parsed (what nodes the parser is inside)
     in_nodes = []
-    out_dict = OrderedDict()
-    # key_read contains the start and end index of the key being read
-    key_read = [0, None]
+    out_dict: Dict = {}
+    # key_read contains the start index of the key being read
+    key_read = 0
     # value_read contains the start index of the value being read
-    value_read = None
-    trigger = set(("\n", "}", "="))
+    value_read = 0
     for index, char in enumerate(data):
         # check if the char is one of the chars which leads to an action
         # this is an optimisation only
-        if char in trigger:
+        if char in {"\n", "}", "="}:
             if char == "\n":
-                # if the key is empty, continue
-                if key_read[0] == index - 1:
-                    pass
-                # if next char is an open bracket, save it as a new node
-                else:
+                # if the last character wasn't a bracket, we have a node
+                if data[index - 1] not in {"{", "}"}:
+                    # if next char is an open bracket, save it as a new node
                     if data[index + 1] == "{":
-                        in_nodes.append(data[key_read[0]: index])
-                        write_list = in_nodes[:]
-                        write_list.append(OrderedDict())
+                        in_nodes.append(data[key_read:index])
+                        set_value(out_dict, in_nodes, {})
                     # else it is a value in an existing node
                     else:
                         write_list = in_nodes[:]
-                        write_list.append(data[key_read[0]: key_read[1]])
-                        write_list.append(data[value_read: index])
-                    set_value(out_dict, write_list)
-                key_read = [index + 1, None]
+                        # use value_read - 1 as the endpoint as the key must end 2 chars before the value starts
+                        write_list.append(data[key_read : value_read - 1])
+                        set_value(out_dict, write_list, data[value_read:index])
+                # read the key from the beginning of the next line
+                key_read = index + 1
             # pop the end of the 'stack' used to track attribute location
             # when the end of a node is found
             elif char == "}":
                 in_nodes.pop()
-            # set the end index of the key and start index of the value
-            # due to the set check (with 'trigger'), the char must be =
+            # due to the set check, the char must be =
+            # set the start index of the value (the end index of the key must be one less than this)
             else:
-                key_read[1] = index - 1
-                value_read = index + 2
+                value_read = index + 1
     return out_dict
 
-def set_value(dict_nested, address_list):
+
+def set_value(
+    dict_nested: Dict, address_list: List[str], value: Union[str, Dict]
+) -> None:
     """Sets a value in a nested dict
     WARNING - modifies the dictionary passed as an arg"""
     # references the main dict
     current = dict_nested
     # locate the desired node to write to through iterating through the keys
     # while selecting the last element of any list found, as the data is in order
-    for path_item in address_list[:-2]:
+    for path_item in address_list[:-1]:
         if isinstance(current, list):
             current = current[-1][path_item]
         else:
@@ -109,24 +110,27 @@ def set_value(dict_nested, address_list):
     if isinstance(current, list):
         current = current[-1]
     # if the node already exists
-    if address_list[-2] in current:
+    prev_node = address_list[-1]
+    if prev_node in current:
         # if it's a list simply append it to the list
-        if isinstance(current[address_list[-2]], list):
-            current[address_list[-2]].append(address_list[-1])
+        if isinstance(current[prev_node], list):
+            current[prev_node].append(value)
         # else convert the existing dict to a list
         else:
-            current[address_list[-2]] = [current[address_list[-2]], address_list[-1]]
+            current[prev_node] = [current[prev_node], value]
     # if it doesn't exist
     else:
         # guaranteed to be a dict thanks to earlier list check, so insert the key into the dict
-        current[address_list[-2]] = address_list[-1]
+        current[prev_node] = value
 
 
-def writeout_savefile(parsed_data, destination_file=""):
+def writeout_savefile(
+    parsed_data: Dict, destination_file: Optional[str] = None
+) -> Optional[str]:
     """Writes out the parsed data back into the SFS format
 
     Params:
-        parsed_data: str; the parsed dictionary generated by parse_savefile.
+        parsed_data: dict; the parsed dictionary generated by parse_savefile.
         destination_file (optional): str; the destination file to write the SFS to.
     Raises:
         No specific exceptions
@@ -138,61 +142,60 @@ def writeout_savefile(parsed_data, destination_file=""):
         the data is not modified. All abnormalities of the SFS format are addressed and
         represented correctly.
     """
-    indents = -1
-    out_str = serialise_data(parsed_data, indents)
+    out_data: List[str] = []
+    serialise_data(parsed_data, out_data, -1)
+    out_str = "".join(out_data)
     if not destination_file:
         return out_str
-    open(destination_file, "w").write(out_str)
+    with open(destination_file, "w") as file:
+        file.write(out_str)
     return None
 
-def serialise_data(obj, indents, outer_key=None):
+
+def serialise_data(obj: Union[List, Dict], out_data: List[str], indents: int) -> None:
     """Recursively serialises data"""
     # indent upon each recurse
     indents += 1
-    out_str = ""
+    indent_str = "\t" * indents
     # set up the buffer list
-    buffer_list = []
     if isinstance(obj, list):
-        for item in obj:
-            buffer_list.append(item)
+        buffer_list = obj
     else:
-        buffer_list.append(obj)
+        buffer_list = [obj]
     for item in buffer_list:
-        # if it is a string, it is one of SFS stupid same keys
-        # to different values, so just write value to node
-        if isinstance(item, str):
-            out_str += write_value_to_node(indents, outer_key, item)
-        else:
-            # it is a dict, so iterate through
-            for key, value in item.items():
-                # if value is a string, it must be a value to write to a node
-                if isinstance(value, str):
-                    out_str += write_value_to_node(indents, key, value)
-                # if it's a dict, it's another node, so recurse
-                elif isinstance(value, dict):
-                    out_str += write_new_node(indents, key, value)
-                # if it's a list it could be multiple things
-                elif isinstance(value, list):
-                    # if everything in the list is a string, then it is one of the multi
-                    # value nodes (could this be optimised TODO)
-                    if all(isinstance(x, str) for x in value):
-                        out_str += serialise_data(value, indents - 1, outer_key=key)
-                    # else just write out each subdict in the list
-                    else:
-                        for subdict in value:
-                            out_str += write_new_node(indents, key, subdict)
-    return out_str
+        # it is a dict, so iterate through
+        for key, value in item.items():
+            # if value is a string, it must be a value to write to a node
+            if isinstance(value, str):
+                out_data.extend((indent_str, key, " = ", value, "\n"))
+            # if it's a dict, it's another node, so recurse
+            elif isinstance(value, dict):
+                write_new_node(out_data, indent_str, indents, key, value)
+            # if it's a list it could be multiple things
+            else:
+                # if the first element is a string, they will all be
+                # it is a multi value node
+                if isinstance(value[0], str):
+                    # write out each value in the node
+                    for res in value:
+                        out_data.extend((indent_str, key, " = ", res, "\n"))
+                # else just write out each subdict in the list
+                else:
+                    for subdict in value:
+                        write_new_node(out_data, indent_str, indents, key, subdict)
 
-def write_new_node(indents, sect_name, value):
+
+def write_new_node(
+    out_data: List[str],
+    indent_str: str,
+    indents: int,
+    sect_name: str,
+    value: Union[List, Dict],
+) -> None:
     """Write a new node to the SFS"""
     # adds the header
-    out_str = "{0}{1}\n{0}{{\n".format("\t" * indents, sect_name)
+    out_data.extend((indent_str, sect_name, "\n", indent_str, "{\n"))
     # adds data through recursion
-    out_str += serialise_data(value, indents, outer_key=sect_name)
+    serialise_data(value, out_data, indents)
     # closes the block
-    out_str += "{0}}}\n".format("\t" * indents)
-    return out_str
-
-def write_value_to_node(indents, key, value):
-    """Writes a key value pair into a node"""
-    return "{0}{1} = {2}\n".format("\t" * indents, key, value)
+    out_data.extend((indent_str, "}\n"))
